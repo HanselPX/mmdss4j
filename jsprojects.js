65fc66d4debc6cd4abce458df77165fe8b9655ac
tiendaonline <ihansel1603@gmail.com>
2024-10-14 01:33:19 -0500

    Add files via upload

--- jsprojects.js
+++ jsprojects.js
@@ -1,16 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const itemImg = document.querySelector('.item-img img');
-    const overlay = document.getElementById('overlay');
-    const fullImage = document.getElementById('fullImage');
+function showGallery() {
+    document.getElementById("imageModal").style.display = "block";
+}
 
-    // Cuando se hace clic en la imagen pequeña
-    itemImg.addEventListener('click', function() {
-        fullImage.src = this.src; // Establecemos la fuente de la imagen grande
-        overlay.style.display = 'flex'; // Mostramos el overlay
-    });
+function closeGallery() {
+    document.getElementById("imageModal").style.display = "none";
+}
 
-    // Cuando se hace clic en el overlay
-    overlay.addEventListener('click', function() {
-        overlay.style.display = 'none'; // Ocultamos el overlay
-    });
-});
